bde40949ab82ef56aeb7dc0e0c6a01203b631cb1
Natalya <natalka118@yandex.ru>
2023-09-03 19:28:25 +0300

    Семинар4

---
+++ Семинар2/P.py
@@ -0,0 +1,14 @@
+#list_1 = [1, 2, 3, 4, 3, 5]
+#k = 3
+#print(list_1.count(k))
+#print(count(k))
+
+#list_1 = [1, 2, 0, 4, 5]
+#k = 3
+
+# Введите ваше решение ниже
+#def n(list_1, k):
+#    return min(list_1, key=lambda x: abs(k - abs(x)))
+#print(n(list_1, k))
+
+
